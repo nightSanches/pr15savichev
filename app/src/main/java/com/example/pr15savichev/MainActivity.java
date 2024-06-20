d39c1e1e59a7cd360cfecbac1e8289932f842e74
nightSanches <galesavich@gmail.com>
2024-06-20 21:12:02 +0500

    Определение пути. Савичев

--- app/src/main/java/com/example/pr15savichev/MainActivity.java
+++ app/src/main/java/com/example/pr15savichev/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pr15savichev;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Environment;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -18,12 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        fileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/record.3gpp";
     }
 }

51071250a01efe8eea3b6a963e47a1d0856838c4
nightSanches <galesavich@gmail.com>
2024-06-20 21:17:20 +0500

    Создание метода recordStart. Савичев

--- app/src/main/java/com/example/pr15savichev/MainActivity.java
+++ app/src/main/java/com/example/pr15savichev/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
         if(mediaRecorder != null){
             mediaRecorder.release();
             mediaRecorder = null;
+        }
+    }
+    public void recordStart(View view){
+        try{
+            releaseRecorder();
+            File outFile = new File(fileName);
+            if(outFile.exists()) outFile.delete();
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(fileName);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 }

cab5c45bb271378c49bf0ab7d922d0c42660e5ca
nightSanches <galesavich@gmail.com>
2024-06-20 21:11:02 +0500

    Объявление переменных. Савичев

--- app/src/main/java/com/example/pr15savichev/MainActivity.java
+++ app/src/main/java/com/example/pr15savichev/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.pr15savichev;
 
 import android.os.Bundle;
-
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +10,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
